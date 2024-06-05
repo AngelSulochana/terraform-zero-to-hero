cbbeb7dac519870523e3a5951a64bc935f855b33
AngelSulochana <90322278+AngelSulochana@users.noreply.github.com>
2024-06-05 11:22:37 +0530

    Create instances.tf

---
+++ modules/instances/instances.tf
@@ -0,0 +1,47 @@
+resource "google_compute_instance" "tf-instance-1" {
+    name = "tf-instance-1"
+  machine_type = "e2-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-11-bullseye-v20240515"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script =  <<-EOT
+        #!/bin/bash
+    EOT
+
+  allow_stopping_for_update = true
+}
+
+resource "google_compute_instance" "tf-instance-2" {
+    name = "tf-instance-2"
+  machine_type = "e2-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-11-bullseye-v20240515"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script =  <<-EOT
+        #!/bin/bash
+    EOT
+
+  allow_stopping_for_update = true
+}
