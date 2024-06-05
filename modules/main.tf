354397ef152d60f340e229dad969da1b4269f536
AngelSulochana <90322278+AngelSulochana@users.noreply.github.com>
2024-06-05 11:21:43 +0530

    Create main.tf

---
+++ modules/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.32.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+module "instance" {
+  source = "./modules/instances"
+  
+}
